d263368e10c51deb1b9f70514b501eeae4d583cb
Egatmm <thanatosr2@gmail.com>
2023-03-25 22:43:07 +0300

    edit home page

--- makkarcle/pages/views.py
+++ makkarcle/pages/views.py
@@ -1,6 +1,12 @@
 from django.views.generic import TemplateView
+from products.models import Product
 
 
-# Create your views here.
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        last_products = Product.objects.order_by('-id')[:3]
+        context['last_products'] = last_products
+        return context
