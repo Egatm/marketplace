306c32efc12cbb62dc3a33b6b4364095e39963f8
Egatmm <thanatosr2@gmail.com>
2023-04-02 16:22:56 +0300

    try deploy

--- makkarcle/makkarcle/settings.py
+++ makkarcle/makkarcle/settings.py
@@ -143,8 +143,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR.joinpath("static/")]
-# STATIC_ROOT = BASE_DIR.joinpath("staticfiles/")
+STATICFILES_DIRS = [BASE_DIR.joinpath("static/")]
+STATIC_ROOT = BASE_DIR.joinpath("staticfiles/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
